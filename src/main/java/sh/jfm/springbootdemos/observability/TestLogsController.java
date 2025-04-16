4e0c8cf23c79ae89e3cfcc9bf19ee6489ec735af
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-04-16 11:05:27 -0500

    Add random log generator

--- src/main/java/sh/jfm/springbootdemos/observability/TestLogsController.java
+++ src/main/java/sh/jfm/springbootdemos/observability/TestLogsController.java
@@ -3,7 +3,10 @@ package sh.jfm.springbootdemos.observability;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.IntStream;
 
 @RestController
 public class TestLogsController {
@@ -13,5 +16,18 @@ public class TestLogsController {
     @PostMapping("/logs/info")
     public void generateTestInfoLogs() {
         log.info("Test info log");
+    }
+    
+    @PostMapping("/logs/random")
+    public void generateRandomLogs(@RequestParam(defaultValue = "10") int lines) {
+        IntStream.range(0, lines).forEach(i -> {
+            String message = "Random log message " + (i + 1);
+            switch ((int) (Math.random() * 4)) {
+                case 0 -> log.info(message);
+                case 1 -> log.debug(message);
+                case 2 -> log.warn(message);
+                case 3 -> log.error(message);
+            }
+        });
     }
 }
