0be2209bda7ca8d8258b66f33fd766e5e29a6d1b
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-04-18 15:40:36 -0500

    Remove unused test config and dependencies

--- src/test/java/sh/jfm/springbootdemos/observability/ObservabilityDemoApplicationTests.java
+++ src/test/java/sh/jfm/springbootdemos/observability/ObservabilityDemoApplicationTests.java
@@ -2,9 +2,7 @@ package sh.jfm.springbootdemos.observability;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ObservabilityDemoApplicationTests {
 
--- src/test/java/sh/jfm/springbootdemos/observability/TestcontainersConfiguration.java
+++
@@ -1,8 +0,0 @@
-package sh.jfm.springbootdemos.observability;
-
-import org.springframework.boot.test.context.TestConfiguration;
-
-@TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-}
