70b3156a9133ca5d65498c357f6c2791ce1b86d5
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-29 10:47:02 -0500

    Remove space

--- src/main/java/sh/jfm/springbootdemos/observability/WebSecurityConfig.java
+++ src/main/java/sh/jfm/springbootdemos/observability/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package sh.jfm.springbootdemos.observability;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
