bb49bd761aea68afd97c07e7cced85a0a2cf40b3
James McMahon <JamesMcMahon@users.noreply.github.com>
2025-05-29 10:39:14 -0500

    Remove additional metric

--- src/main/java/sh/jfm/springbootdemos/observability/TestSecurityController.java
+++ src/main/java/sh/jfm/springbootdemos/observability/TestSecurityController.java
@@ -1,6 +1,5 @@
 package sh.jfm.springbootdemos.observability;
 
-import io.micrometer.observation.annotation.Observed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,7 +11,6 @@ public class TestSecurityController {
 
     private static final Logger logger = LoggerFactory.getLogger(TestSecurityController.class);
 
-    @Observed(name = "secure", contextualName = "secure-endpoint")
     @GetMapping("/secure")
     @PreAuthorize("isAuthenticated()")
     public String securedEndpoint() {
